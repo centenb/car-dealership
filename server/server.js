f9911c699af344e1a990619e85509322a2943ce4
centenb <akbingo@mail.com>
2025-11-01 13:00:00 -0400

    render

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import employees from './routes/employee.js';
 import dotenv from 'dotenv';
+import path from 'path';
 dotenv.config();
 
 const app = express();
