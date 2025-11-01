22ece4c1693946617da602ee031deaa89d4fdabd
centenb <akbingo@mail.com>
2025-11-01 14:33:56 -0400

    render

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export const Navbar = () => {
   return (
-    <div>Navbar
+    <div className="flex justify-between p-4">
+      Navbar
         <div>
-            <a href="/home">Home</a>
-            <a href="/">Login</a>
+            <Link to="/home">Home</Link>
+            <Link to="/">Login</Link>
         </div>
     </div>
   )
