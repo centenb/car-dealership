7983c93d3b831dffbee53cdd76d9d33a5dd87908
centenb <akbingo@mail.com>
2025-11-01 13:44:21 -0400

    render

--- server/server.js
+++ server/server.js
@@ -9,13 +9,13 @@ dotenv.config();
 const app = express();
 
 //middleware
+app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 app.use("/employees", employees);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(process.cwd(), 'dist')))
 }
-app.use(morgan('tiny'));
 
 app.listen(5000, () => {
     console.log("Express server has started on port 5000")
